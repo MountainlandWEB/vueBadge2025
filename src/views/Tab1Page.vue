382066ef144eaa18ad126b12e44b31298a2e6304
Todd H <github@simplybest.com>
2025-05-07 09:40:49 -0600

    add badge code for tabs and browser pwa icon

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -10,7 +10,9 @@
       <ion-input label="Badge count" type="number" :clear-input="true" 
         v-model="badgeCount"></ion-input>
 
-      <ion-button expand="full" color="primary" @click="badgeCount = 0">
+      <ion-button expand="full" color="primary" @click="setBadgeCount">
+        Set Badge Count</ion-button>
+      <ion-button expand="full" color="primary" @click="clearBadgeCount">
         Mark as Read</ion-button>
 
     </ion-content>
@@ -22,4 +24,20 @@ import { IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInp
 import { ref } from 'vue';
 
 const badgeCount = ref(3);
+
+const clearBadgeCount = async () => {
+  badgeCount.value = 0;
+  await setBadgeCount();
+};
+
+const setBadgeCount = async () => {
+  if (badgeCount.value === 0) {
+    navigator.clearAppBadge && await navigator.clearAppBadge();
+  } else {
+    navigator.setAppBadge && await navigator.setAppBadge(badgeCount.value)
+  }
+  const event = new CustomEvent("badgeCount", { detail: {badgeCount: badgeCount.value} });
+  window.dispatchEvent(event);
+};
+
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,6 +4,7 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-badge v-if="badgeCount > 0" color="danger">{{ badgeCount }}</ion-badge>
           <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
@@ -23,6 +24,23 @@
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { IonBadge, IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
+import { onUnmounted, ref } from 'vue';
+
+// consume the badge count from the child component
+
+const badgeCount = ref(3);
+
+const onBadgeCount = (e: any) => updateBadgeCount(e.detail.badgeCount)
+
+window.addEventListener('badgeCount', onBadgeCount)
+
+onUnmounted(() => {
+  window.removeEventListener('badgeCount', onBadgeCount);
+})
+
+const updateBadgeCount = (count: number) => {
+  badgeCount.value = count;
+}
 </script>
